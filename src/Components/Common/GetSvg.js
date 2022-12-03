f751f6efc02cf3cadad530c89697d7ec5a52b730
savrabh.flairlabs <savrabh.flairlabs@ext.idfy.com>
2022-12-03 11:27:13 +0530

    Media Query

--- src/Components/Common/GetSvg.js
+++ src/Components/Common/GetSvg.js
@@ -18,8 +18,8 @@ const GetSvg = (param) => {
     case "magnify":
       return (
         <svg
-          width="22"
-          height="22"
+          width="20"
+          height="20"
           viewBox="0 0 22 22"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
